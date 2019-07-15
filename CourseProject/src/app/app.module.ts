adc8ebf3f0a49e54dba640dd4296d6ac3d770ae4
Илия Митвичин <ilia@kodar.net>
2019-07-15 10:47:12 +0300

    finished Http section

--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './Directives/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     FormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
   ],
